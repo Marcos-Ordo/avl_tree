e80cad09ec00754cfc09dd6b51bdb42518dc9110
marcos-ordo <ordonezmarcos935@gmail.com>
2026-05-31 15:17:56 -0300

    forgot to put deleteAVL to the tests

--- test/Main.hs
+++ test/Main.hs
@@ -34,20 +34,20 @@ test03_whenInsertingExtras =
 
 test04_whenDeleting_CheckSingleRotations :: IO ()
 test04_whenDeleting_CheckSingleRotations =
-    do check "DEL 4 (INS [3,4,2,1]) (LL)" ((deleteAVL 4 . ins) [3,4,2,1])
-       check "DEL 1 (INS [2,1,3,4]) (RR)" ((deleteAVL 1 . ins) [2,1,3,4])
+    do check "DEL 4 (INS [3,4,2,1]) (LL)" (deleteAVL 4 $ ins [3,4,2,1])
+       check "DEL 1 (INS [2,1,3,4]) (RR)" (deleteAVL 1 $ ins [2,1,3,4])
 
 test05_whenDeleting_CheckDoubleRotations :: IO ()
 test05_whenDeleting_CheckDoubleRotations =
-    do check "DEL 4 (INS [3,4,1,2]) (LR)" (ins [3,4,1,2])
-       check "DEL 1 (INS [3,4,1,2]) (RL)" (ins [3,4,1,2])
+    do check "DEL 4 (INS [3,4,1,2]) (LR)" (deleteAVL 4 $ ins [3,4,1,2])
+       check "DEL 1 (INS [3,4,1,2]) (RL)" (deleteAVL 1 $ ins [3,4,1,2])
 
 test06_whenDeletingExtras :: IO ()
 test06_whenDeletingExtras =
-    do check "DEL 1  (INS [1..5])"                    (ins [1..5])
-       check "DEL 4  (INS [5,4..1])"                  (ins [5,4..1])
-       check "DEL 3  (INS [5,2,4,1,3])"               (ins [5,2,4,1,3])
-       check "DEL 33 (INS [10,15,5,33,7,12,20,11,4])" (ins [10,15,5,33,7,12,20,11,4])
+    do check "DEL 1  (INS [1..5])"                    (deleteAVL 1  $ ins [1..5])
+       check "DEL 4  (INS [5,4..1])"                  (deleteAVL 4  $ ins [5,4..1])
+       check "DEL 3  (INS [5,2,4,1,3])"               (deleteAVL 3  $ ins [5,2,4,1,3])
+       check "DEL 33 (INS [10,15,5,33,7,12,20,11,4])" (deleteAVL 33 $ ins [10,15,5,33,7,12,20,11,4])
 
 test07_whenTryingToDoFold_ItWorks :: IO ()
 test07_whenTryingToDoFold_ItWorks = 
